24fa3f2439317997cad230df5945dc29ba7335bb
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-11-21 23:37:44 -0300

    Database User and Network Access parte 1

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ import mongoose from 'mongoose';
 //App Config
 const app = express();
 const port = process.env.PORT || 8001;
+const connection_url =
+  'mongodb+srv://admin:yourpassword@cluster0.lggjc.mongodb.net/datingDB?retryWrites=true&w=majority';
 //Middleware
+
 //DB Config
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
 //API Endpoints
 app.get('/', (req, res) => res.status(200).send('Hello TheWebDev'));
 //Listener
