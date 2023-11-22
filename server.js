db33e72542089de20dc2d228e0bc1ac775a11e69
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-11-22 19:28:05 -0300

    fazendo requisição post com sucesso

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import Cards from './dbCards.js';
+import Cors from 'cors';
 //App Config
 const app = express();
 const port = process.env.PORT || 8001;
 const connection_url =
   'mongodb+srv://admin:91537035Hugo@cluster0.0hxrude.mongodb.net/?retryWrites=true&w=majority';
+
 //Middleware
+app.use(express.json());
+app.use(Cors());
 
 //DB Config
 mongoose.connect(connection_url, {});
@@ -14,16 +18,27 @@ mongoose.connect(connection_url, {});
 //API Endpoints
 app.get('/', (req, res) => res.status(200).send('Hello TheWebDev'));
 
+/**
+ * M_User.create(data)
+.then((result) => {
+  res.send({ kq: 1, msg: 'Đã thêm thành công' })
+})
+.catch((err) => {
+  res.send({ kq: 0, msg: 'kết nối DB thất bại' })
+})
+ */
+
 app.post('/dating/cards', (req, res) => {
-  const dbCard = req.body;
-  Cards.create(dbCard, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
+  const data = req.body;
+  Cards.create(data)
+    .then((result) => {
       res.status(201).send(data);
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
 });
+
 app.get('/dating/cards', (req, res) => {
   Cards.find((err, data) => {
     if (err) {
